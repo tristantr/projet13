d7a47e600f0adea1863e293002bb4dfdb98b2ed2
Tristan Lecuyer <tristan.use@gmail.com>
2021-05-20 15:49:09 +0200

    Add Boostrap template

--- projet_13/urls.py
+++ projet_13/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('', include('places.urls'))
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
