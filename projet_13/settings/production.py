7726cf2b21f168476267ca3b5aeb1d8e6e20c9d0
Tristan Lecuyer <tristan.use@gmail.com>
2021-06-03 12:03:26 +0200

    Correct mistake config env

--- projet_13/settings/production.py
+++ projet_13/settings/production.py
@@ -23,5 +23,3 @@ DATABASES = {
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
